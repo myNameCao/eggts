631831c7f59f55bf66ffc24d5dcbaee152949b44
caohefei <hefei.cao@xiaoyangedu.com>
2022-05-19 15:42:39 +0800

    chore: merge code

--- config/config.default.ts
+++ config/config.default.ts
@@ -1,23 +1,45 @@
-import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
+import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg'
 
 export default (appInfo: EggAppInfo) => {
-  const config = {} as PowerPartial<EggAppConfig>;
+  const config = {} as PowerPartial<EggAppConfig>
 
   // override config from framework / plugin
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1652866071961_302';
+  config.keys = appInfo.name + '_1652866071961_302'
+
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks',
+      '.html': 'nunjucks'
+    }
+  }
+
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks',
+      '.html': 'nunjucks'
+    }
+  }
+
+  config.security = {
+    csrf: {
+      headerName: 'x-csrf-token' // 通过 header 传递 CSRF token 的默认字段为 x-csrf-token
+    }
+  }
 
   // add your egg config in here
-  config.middleware = [];
+  config.middleware = []
 
   // add your special config in here
   const bizConfig = {
-    sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
-  };
+    sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`
+  }
 
   // the return config will combines to EggAppConfig
   return {
     ...config,
-    ...bizConfig,
-  };
-};
+    ...bizConfig
+  }
+}
