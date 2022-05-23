e5cb8a7056b13471a818ab36d69ebed2d35bfe48
hefei.cao <hefei.cao@xiaoyangedu.com>
2022-05-23 10:34:25 +0800

    build: http://chris.xycxedu.cn:7001/项目正常运行

--- config/config.default.ts
+++ config/config.default.ts
@@ -2,7 +2,7 @@
  * @Author: hefei.cao hefei.cao@xiaoyangedu.com
  * @Date: 2022-05-20 17:11:19
  * @LastEditors: hefei.cao hefei.cao@xiaoyangedu.com
- * @LastEditTime: 2022-05-21 23:39:18
+ * @LastEditTime: 2022-05-23 10:28:27
  * @FilePath: \eggts\config\config.default.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,6 +10,8 @@ import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg'
 
 export default (appInfo: EggAppInfo) => {
   const config = {} as PowerPartial<EggAppConfig>
+
+  console.log(config)
 
   // override config from framework / plugin
   // use for cookie sign key, should change to your own and keep security
@@ -45,6 +47,7 @@ export default (appInfo: EggAppInfo) => {
   }
 
   // the return config will combines to EggAppConfig
+
   return {
     ...config,
     ...bizConfig
--- config/config.prod.ts
+++ config/config.prod.ts
@@ -1,6 +1,14 @@
-import { EggAppConfig, PowerPartial } from 'egg';
+/*
+ * @Author: hefei.cao hefei.cao@xiaoyangedu.com
+ * @Date: 2022-05-21 23:19:12
+ * @LastEditors: hefei.cao hefei.cao@xiaoyangedu.com
+ * @LastEditTime: 2022-05-23 08:30:51
+ * @FilePath: \eggts\config\config.prod.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { EggAppConfig, PowerPartial } from 'egg'
 
 export default () => {
-  const config: PowerPartial<EggAppConfig> = {};
-  return config;
-};
+  const config: PowerPartial<EggAppConfig> = {}
+  return config
+}
