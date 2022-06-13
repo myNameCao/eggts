4c72af8c1f6deba5a7f8ddb6c402e4101c365c4a
caohefei <hefei.cao@xiaoyangedu.com>
2022-06-13 17:01:00 +0800

    chore: 添加接口swagger

--- app/controller/answersheet.ts
+++ app/controller/answersheet.ts
@@ -1,6 +1,17 @@
 import { Controller } from 'egg'
 
+/**
+ * @controller answersheetController
+ */
 export default class answersheetController extends Controller {
+  /**
+   *@router get /api/answerSheet/list  路径
+   *@summary 答题卡列表
+   *@description 获取答题卡的列表
+   *@request query integer pageSize 页容量
+   *@request query string pageNumber 当前页码
+   *@response 200 indexJsonBody
+   */
   public async getList() {
     const listRule = {
       pageSize: 'string',
@@ -12,21 +23,52 @@ export default class answersheetController extends Controller {
     const result = await service.answersheet.getList()
     ctx.setBody(result)
   }
+
+  /**
+   *@router get /api/answerSheet/getDetail  路径
+   *@summary 答题卡详情
+   *@description 获取某一个答题卡的详情
+   *@request query integer asId 答题卡id
+   *@response 200 indexJsonBody
+   */
   public async getDetail() {
     const { ctx, service } = this
     const result = await service.answersheet.getDetail()
     ctx.setBody(result)
   }
+
+  /**
+   *@router delete /api/answerSheet/delete  路径
+   *@summary 答题卡删除
+   *@description 删除某一个答题卡
+   *@request query integer asId 答题卡id
+   *@response 200 indexJsonBody
+   */
   public async delete() {
     const { ctx, service } = this
     const result = await service.answersheet.delcard()
     ctx.setBody(result)
   }
+  /**
+   *@router post /api/answerSheet/savecard  路径
+   *@summary 答题卡保存
+   *@description 保存某一个答题卡
+   *@request param model body  答题卡信息
+   *@response 200 indexJsonBody
+   */
   public async savecard() {
     const { ctx, service } = this
     const result = await service.answersheet.savecard()
     ctx.setBody(result)
   }
+
+  /**
+   *@router get /api/answerSheet/card  路径
+   *@summary 答题卡下载
+   *@description 下载某一个答题卡
+   *@request query integer asId 答题卡id
+   *@response 200 indexJsonBody
+   */
   public async download() {
     const { ctx, service } = this
     const result = await service.answersheet.download()
--- app/controller/home.ts
+++ app/controller/home.ts
@@ -1,6 +1,15 @@
 import { Controller } from 'egg'
 
+/**
+ * @controller HomeController
+ */
 export default class HomeController extends Controller {
+  /**
+   *@router get /index 路径
+   *@summary 页面
+   *@description 测试页面
+   *@response 200 html
+   */
   public async index() {
     const { ctx } = this
     return ctx.render('home.html')
