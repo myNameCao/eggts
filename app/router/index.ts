735d74284f239e6977b404694c75db32c33a654d
hefei.cao <hefei.cao@xiaoyangedu.com>
2022-06-14 05:49:01 +0800

    fix: 配置swagger 页面

--- app/router/index.ts
+++ app/router/index.ts
@@ -5,8 +5,9 @@ import examApi from './exam'
 import answersheetApi from './answersheet'
 
 export default (app: Application) => {
-  const { controller, router } = app
-  router.get('/', controller.home.index)
+  const { router } = app
+  // router.get('/', controller.home.index)
+  router.redirect('/', '/swagger-ui.html', 302)
   testApi(app)
   examApi(app)
   answersheetApi(app)
